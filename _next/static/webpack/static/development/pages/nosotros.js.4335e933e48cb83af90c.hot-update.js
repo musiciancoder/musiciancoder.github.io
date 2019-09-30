c329fbdbe8ecfddf9a47ec11f6812a57ab3bdd89
musiciancoder <andresguitarra1980@gmail.com>
2019-09-30 14:25:36 -0300

    primer comit

---
+++ _next/static/webpack/static/development/pages/nosotros.js.4335e933e48cb83af90c.hot-update.js
@@ -0,0 +1,79 @@
+webpackHotUpdate("static\\development\\pages\\nosotros.js",{
+
+/***/ "./pages/nosotros.js":
+/*!***************************!*\
+  !*** ./pages/nosotros.js ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_Master__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/Master */ "./components/Master.js");
+var _jsxFileName = "C:\\untitled\\bitcoinapp\\pages\\nosotros.js";
+
+
+
+var Nosotros = function Nosotros() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Master__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "row text-center",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, "Andr\xE9s D\xEDaz says hello to his greats clients all around the world!")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "row text-center",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, "Andr\xE9s D\xEDaz te manda saludos!")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "row text-center",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, "E a\xED galera, eu sou o Andr\xE9s. Abra\xE7\xE3o!"))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Nosotros);
+
+/***/ })
+
+})
+//# sourceMappingURL=nosotros.js.4335e933e48cb83af90c.hot-update.js.map
